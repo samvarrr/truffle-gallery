c185cd6df12fa69c3bfc587087f8f0a12a4f3f64
samvarrr <karuna.valeriya@gmail.com>
2026-05-24 23:20:36 +0300

    Delete Script1.js

--- Script1.js
+++
@@ -1,8 +0,0 @@
-const images = document.querySelectorAll('.gallery-img');
-
-images.forEach(img => {
-    img.addEventListener('click', () => {
-        // toggle המהא÷ ךכאס, ÿךשמ ימדמ םולא÷, ³ גטהאכÿ÷, ÿךשמ ג³ם ÷
-        img.classList.toggle('active');
-    });
-});
